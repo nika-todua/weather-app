547bc304c09eb93dbdd4d6e5b48025be7db8c28f
nika todua <alchimikos@gmail.com>
2024-11-01 16:04:00 +0400

    update

--- js/script.js
+++ js/script.js
@@ -71,29 +71,33 @@ function weatherDetails(info) {
     const { description, id } = info.weather[0];
     const { temp, feels_like, humidity } = info.main;
 
-    // using custom weather icon according to the id which api gives to us
-    if (id == 800) {
-      wIcon.src = "icons/clear.svg";
-    } else if (id >= 200 && id <= 232) {
-      wIcon.src = "icons/storm.svg";
-    } else if (id >= 600 && id <= 622) {
-      wIcon.src = "icons/snow.svg";
-    } else if (id >= 701 && id <= 781) {
-      wIcon.src = "icons/haze.svg";
-    } else if (id >= 801 && id <= 804) {
-      wIcon.src = "icons/cloud.svg";
-    } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
-      wIcon.src = "icons/rain.svg";
+    switch (true) {
+      case id === 800:
+        wIcon.src = "https://weather-amindi.netlify.app/icons/clear.svg";
+        break;
+      case id >= 200 && id <= 232:
+        wIcon.src = "https://weather-amindi.netlify.app/icons/storm.svg";
+        break;
+      case id >= 600 && id <= 622:
+        wIcon.src = "https://weather-amindi.netlify.app/icons/snow.svg";
+        break;
+      case id >= 701 && id <= 781:
+        wIcon.src = "https://weather-amindi.netlify.app/icons/haze.svg";
+        break;
+      case id >= 801 && id <= 804:
+        wIcon.src = "https://weather-amindi.netlify.app/icons/cloud.svg";
+        break;
+      case (id >= 500 && id <= 531) || (id >= 300 && id <= 321):
+        wIcon.src = "https://weather-amindi.netlify.app/icons/rain.svg";
+        break;
     }
+    
 
     //passing a particular weather info to a particular element
     weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
     weatherPart.querySelector(".weather").innerText = description;
-    weatherPart.querySelector(
-      ".location span"
-    ).innerText = `${city}, ${country}`;
-    weatherPart.querySelector(".temp .numb-2").innerText =
-      Math.floor(feels_like);
+    weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+    weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
     weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
     infoTxt.classList.remove("pending", "error");
     infoTxt.innerText = "";
